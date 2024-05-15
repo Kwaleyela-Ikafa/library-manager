1eae20946dfa138203e0d271772a7cd5e6833b1b
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-15 23:33:27 +0200

    Add getters and setters to book class

--- src/Book.java
+++ src/Book.java
@@ -6,6 +6,38 @@ public class Book implements LibraryItem {
     private String genre;
     private String availability;
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
     @Override
     public void checkOut() {
 
