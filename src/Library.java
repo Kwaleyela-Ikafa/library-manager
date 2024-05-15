4ffe649a53180a61ac2030cb02e6d20a62a4f937
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-15 23:32:18 +0200

    Create Library class to manage all books

---
+++ src/Library.java
@@ -0,0 +1,29 @@
+import interfaces.LibraryItem;
+
+public class Library implements LibraryItem {
+    public void addBook(Book book) {
+
+    }
+
+    public void removeBook(Book book) {
+
+    }
+
+    public Book searchBook(String book) {
+        return null;
+    }
+
+    public Book getBooks() {
+        return null;
+    }
+
+    @Override
+    public void checkOut() {
+
+    }
+
+    @Override
+    public void checkIn() {
+
+    }
+}
