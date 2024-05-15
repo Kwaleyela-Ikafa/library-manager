1d391c5728ed9225ba3faf5e5a732e961ae4640c
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-15 23:31:31 +0200

    Create Book class

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+import interfaces.LibraryItem;
+
+public class Book implements LibraryItem {
+    private String title;
+    private String author;
+    private String genre;
+    private String availability;
+
+    @Override
+    public void checkOut() {
+
+    }
+
+    @Override
+    public void checkIn() {
+
+    }
+}
