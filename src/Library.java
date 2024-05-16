3ca8ea4434849559dc80a8bcc5de96dafe11fc42
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-05-16 12:24:18 +0200

    Update CRUD operations

--- src/Library.java
+++ src/Library.java
@@ -1,26 +1,74 @@
+import enums.Genre;
 import interfaces.LibraryItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    public void addBook(Book book) {
 
+    List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        this.books.add(book);
     }
 
     public void removeBook(Book book) {
-
+        this.books.remove(book);
     }
 
-    public String searchBookByTitle(String title) {
+    public Book searchBookByTitle(String title) {
+        for(Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(title) && !book.isAvailable()) {
+                return book;
+            }
+        }
         return null;
     }
 
-    public String searchBookByAuthor(String author) {
+    public Book searchBookByAuthor(String author) {
+        for(Book book : books) {
+            if(book.getAuthor().equalsIgnoreCase(author) && !book.isAvailable()) {
+                return book;
+            }
+        }
         return null;
     }
 
     public List<Book> displayAvailableBooks() {
-        return null;
+        List<Book> availableBooks = new ArrayList<>();
+        for(Book book : books) {
+            if(book.isAvailable()) {
+                availableBooks.add(book);
+            }
+        }
+        return availableBooks;
     }
 
+    public void checkInBook(String title) {
+        for (Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(title)) {
+                if(!book.isAvailable()) { // Check if the book is not already available
+                    book.setAvailable(true); // Set the book as available
+                    System.out.println("Book '" + title + "' has been checked in successfully.");
+                } else {
+                    System.out.println("Book '" + title + "' is already checked in.");
+                    return;
+                }
+            }
+        }
+    }
+
+    public void checkOutBook(String title) {
+        for (Book book : books) {
+            if(book.getTitle().equalsIgnoreCase(title)) {
+                if(book.isAvailable()) { // Check if the book is already available
+                    book.setAvailable(false); // Set the book as not available
+                    System.out.println("Book '" + title + "' has been checked Out successfully.");
+                } else {
+                    System.out.println("Book '" + title + "' is already checked Out.");
+                    return;
+                }
+            }
+        }
+    }
 }
